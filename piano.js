710798959ce339f226e2cf759866468a1a075e6c
Parth Ardeshana <69377041+parthardeshana@users.noreply.github.com>
2020-10-25 13:30:12 +0530

    Add files via upload

---
+++ piano.js
@@ -0,0 +1,88 @@
+console.log('this is Piano website');
+
+let tone1 = new Audio('tone/1.mp3');
+let tone2 = new Audio('tone/2.mp3');
+let tone3 = new Audio('tone/3.mp3');
+let tone4 = new Audio('tone/4.mp3');
+let tone5 = new Audio('tone/5.mp3');
+let tone6 = new Audio('tone/6.mp3');
+let tone7 = new Audio('tone/7.mp3');
+let tone8 = new Audio('tone/8.mp3');
+let tone9 = new Audio('tone/9.mp3');
+let tone10 = new Audio('tone/10.mp3');
+let tone11 = new Audio('tone/11.mp3');
+let tone12 = new Audio('tone/12.mp3');
+let tone13 = new Audio('tone/13.mp3');
+let tone14 = new Audio('tone/14.mp3');
+let tone15 = new Audio('tone/15.mp3');
+
+// White buttons get from DOM
+let w1 = document.getElementById('w1');
+let w2 = document.getElementById('w2');
+let w3 = document.getElementById('w3');
+let w4 = document.getElementById('w4');
+let w5 = document.getElementById('w5');
+let w6 = document.getElementById('w6');
+let w7 = document.getElementById('w7');
+let w8 = document.getElementById('w8');
+let w9 = document.getElementById('w9');
+let w10 = document.getElementById('w10');
+let w11 = document.getElementById('w11');
+let w12 = document.getElementById('w12');
+let w13 = document.getElementById('w13');
+let w14 = document.getElementById('w14');
+let w15 = document.getElementById('w15');
+
+// Black buttons get from DOM
+
+let b1 = document.getElementById('b1');
+let b2 = document.getElementById('b2');
+let b3 = document.getElementById('b3');
+let b4 = document.getElementById('b4');
+let b5 = document.getElementById('b5');
+let b6 = document.getElementById('b6');
+let b7 = document.getElementById('b7');
+let b8 = document.getElementById('b8');
+let b9 = document.getElementById('b9');
+let b10 = document.getElementById('b10');
+let b11 = document.getElementById('b11');
+let b12 = document.getElementById('b12');
+let b13 = document.getElementById('b13');
+let b14 = document.getElementById('b14');
+
+
+//Black button event starts here 
+w1.addEventListener('click', ()=> { tone1.play(); });
+w2.addEventListener('click', ()=> { tone2.play(); });
+w3.addEventListener('click', ()=> { tone3.play(); });
+w4.addEventListener('click', ()=> { tone4.play(); });
+w5.addEventListener('click', ()=> { tone5.play(); });
+w6.addEventListener('click', ()=> { tone6.play(); });
+w7.addEventListener('click', ()=> { tone7.play(); });
+w8.addEventListener('click', ()=> { tone8.play(); });
+w9.addEventListener('click', ()=> { tone9.play(); });
+w10.addEventListener('click', ()=> { tone10.play(); });
+w11.addEventListener('click', ()=> { tone11.play(); });
+w12.addEventListener('click', ()=> { tone12.play(); });
+w13.addEventListener('click', ()=> { tone13.play(); });
+w14.addEventListener('click', ()=> { tone14.play(); });
+w15.addEventListener('click', ()=> { tone15.play(); });
+
+//Black button event starts here 
+b1.addEventListener('click', ()=> { tone1.play(); });
+b2.addEventListener('click', ()=> { tone2.play(); });
+b3.addEventListener('click', ()=> { tone3.play(); });
+b4.addEventListener('click', ()=> { tone4.play(); });
+b5.addEventListener('click', ()=> { tone5.play(); });
+b6.addEventListener('click', ()=> { tone6.play(); });
+b7.addEventListener('click', ()=> { tone7.play(); });
+b8.addEventListener('click', ()=> { tone8.play(); });
+b9.addEventListener('click', ()=> { tone9.play(); });
+b10.addEventListener('click', ()=> { tone10.play(); });
+b11.addEventListener('click', ()=> { tone11.play(); });
+b12.addEventListener('click', ()=> { tone12.play(); });
+b13.addEventListener('click', ()=> { tone13.play(); });
+b14.addEventListener('click', ()=> { tone14.play(); });
+
+
+
